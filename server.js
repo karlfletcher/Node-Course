ca1e621d5c7f1fc118bf1449607eab7d0762b5b0
Karl Fletcher <karl.fletcher@q2q-it.com>
2017-01-19 14:11:27 +0000

    Code refactor

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
+//REQUIRES
 var express = require('express');
 var app = express();
+var middleware = require('./middleware.js');
 var _port = 3000;
 
-var middleware = {
-	requireAuthentication	: function(req, res, next){
-		console.log("Private route hit.");
-		next();
-	},
-	logger: function(req, res, next){
-		console.log(new Date().toString() + " - " + req.method + " " + req.originalUrl);
-		next();
-	}
-}
-
+//APP SETUP
 app.use(middleware.logger);
+app.use(express.static(__dirname + '/public'));
 
+//ROUTER
 app.get('/about', middleware.requireAuthentication, function(req, res){
 	res.send("About us.");
 });
-
-app.use(express.static(__dirname + '/public'));
 
 app.listen(_port, function(){
 	console.log("Starting server on port " + _port + ".");
